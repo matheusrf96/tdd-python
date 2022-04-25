cbe41b1c29ae7623419b75b5241a0ded8527737d
Matheus Rodrigues <matheusrf96@gmail.com>
2022-04-25 12:39:07 -0300

    Implement class Avaliador

--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -1,4 +1,4 @@
-from src.leilao.dominio import Usuario, Lance, Leilao
+from src.leilao.dominio import Avaliador, Usuario, Lance, Leilao
 
 user1 = Usuario('User 1')
 user2 = Usuario('User 2')
@@ -12,4 +12,9 @@ leilao.lances.append(lance1)
 leilao.lances.append(lance2)
 
 for lance in leilao.lances:
-    print(f'O usuário { lance.usuario.nome } deu um lance de { lance.valor }.')
+    print(f'O usuário "{ lance.usuario.nome }" deu um lance de "{ lance.valor }".')
+
+ava = Avaliador()
+ava.avalia(leilao)
+
+print(f'O menor lance foi de "{ ava.menor_lance }". O maior lance foi de "{ ava.maior_lance }"')
