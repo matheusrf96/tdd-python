2c19ef6c5cf6a8ef96b6fcd76436aa4f2f62419e
Matheus Rodrigues <matheusrf96@gmail.com>
2022-04-26 15:57:44 -0300

    Isolate test context

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -5,20 +5,24 @@ from unittest import TestCase
 
 
 class TestAvaliador(TestCase):
+
+    def setUp(self):
+        self.user1 = Usuario('User 1')
+        self.user2 = Usuario('User 2')
+        self.user3 = Usuario('User 3')
+
+        self.lance1 = Lance(self.user1, 50.0)
+        self.lance2 = Lance(self.user2, 100.0)
+        self.lance3 = Lance(self.user3, 30.0)
+
+        self.leilao = Leilao('Celular')
+
     def test_avalia_asc(self):
-        user1 = Usuario('User 1')
-        user2 = Usuario('User 2')
-
-        lance1 = Lance(user1, 50.0)
-        lance2 = Lance(user2, 100.0)
-
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance1)
-        leilao.lances.append(lance2)
+        self.leilao.lances.append(self.lance1)
+        self.leilao.lances.append(self.lance2)
 
         ava = Avaliador()
-        ava.avalia(leilao)
+        ava.avalia(self.leilao)
 
         menor_valor_esperado = 50.0
         maior_valor_esperado = 100.0
@@ -27,19 +31,11 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, ava.maior_lance)
 
     def test_avalia_desc(self):
-        user1 = Usuario('User 1')
-        user2 = Usuario('User 2')
-
-        lance1 = Lance(user1, 50.0)
-        lance2 = Lance(user2, 100.0)
-
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance2)
-        leilao.lances.append(lance1)
+        self.leilao.lances.append(self.lance2)
+        self.leilao.lances.append(self.lance1)
 
         ava = Avaliador()
-        ava.avalia(leilao)
+        ava.avalia(self.leilao)
 
         menor_valor_esperado = 50.0
         maior_valor_esperado = 100.0
@@ -48,16 +44,10 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, ava.maior_lance)
 
     def test_avalia_single_lance(self):
-        user1 = Usuario('User 1')
-
-        lance1 = Lance(user1, 50.0)
-
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance1)
+        self.leilao.lances.append(self.lance1)
 
         ava = Avaliador()
-        ava.avalia(leilao)
+        ava.avalia(self.leilao)
 
         menor_valor_esperado = 50.0
         maior_valor_esperado = 50.0
@@ -66,22 +56,12 @@ class TestAvaliador(TestCase):
         self.assertEqual(maior_valor_esperado, ava.maior_lance)
 
     def test_avalia_multiple_lances(self):
-        user1 = Usuario('User 1')
-        user2 = Usuario('User 2')
-        user3 = Usuario('User 3')
-
-        lance1 = Lance(user1, 50.0)
-        lance2 = Lance(user2, 100.0)
-        lance3 = Lance(user3, 30.0)
-
-        leilao = Leilao('Celular')
-
-        leilao.lances.append(lance1)
-        leilao.lances.append(lance2)
-        leilao.lances.append(lance3)
+        self.leilao.lances.append(self.lance1)
+        self.leilao.lances.append(self.lance2)
+        self.leilao.lances.append(self.lance3)
 
         ava = Avaliador()
-        ava.avalia(leilao)
+        ava.avalia(self.leilao)
 
         menor_valor_esperado = 30.0
         maior_valor_esperado = 100.0
