dd9736d20f83d7a1416c1b826661e07f66ffa0a6
Matheus Rodrigues <matheusrf96@gmail.com>
2022-05-02 12:14:05 -0300

    Replace test class name and create tests to check if the same user can bet twice in sequence

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -31,12 +31,13 @@ class Leilao:
         return self.__lances[:]
 
     def propoe(self, lance: Lance):
-        if lance.valor > self.maior_lance:
-            self.maior_lance = lance.valor
-        if lance.valor < self.menor_lance:
-            self.menor_lance = lance.valor
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario:
+            if lance.valor > self.maior_lance:
+                self.maior_lance = lance.valor
+            if lance.valor < self.menor_lance:
+                self.menor_lance = lance.valor
 
-        self.__lances.append(lance)
+            self.__lances.append(lance)
 
 
 class Avaliador:
--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -4,7 +4,7 @@ from src.leilao.dominio import Usuario, Lance, Leilao
 from unittest import TestCase
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     def setUp(self):
         self.user1 = Usuario('User 1')
@@ -14,6 +14,7 @@ class TestAvaliador(TestCase):
         self.lance1 = Lance(self.user1, 50.0)
         self.lance2 = Lance(self.user2, 100.0)
         self.lance3 = Lance(self.user3, 30.0)
+        self.lance4 = Lance(self.user1, 75.0)
 
         self.leilao = Leilao('Celular')
 
@@ -56,3 +57,19 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_create_lance_when_theres_no_lances(self):
+        self.leilao.propoe(self.lance1)
+        self.assertEqual(1, len(self.leilao.lances))
+
+    def test_allow_lance_when_user_is_different_from_the_previous(self):
+        self.leilao.propoe(self.lance1)
+        self.leilao.propoe(self.lance2)
+
+        self.assertEqual(2, len(self.leilao.lances))
+
+    def test_doesnt_allow_lance_from_the_same_user_twice_in_sequence(self):
+        self.leilao.propoe(self.lance1)
+        self.leilao.propoe(self.lance4)
+
+        self.assertEqual(1, len(self.leilao.lances))
