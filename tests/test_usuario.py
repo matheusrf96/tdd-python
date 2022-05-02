4948424ccddfa987fee1e1359a47afdc37a3b795
Matheus Rodrigues <matheusrf96@gmail.com>
2022-05-02 14:25:23 -0300

    Create tests to compare lance value and wallet value and refactor functions

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,10 +1,29 @@
 from src.leilao.dominio import Leilao, Usuario
 
 
-def test_subtract_wallet_value():
-    user1 = Usuario('User 1', 100.0)
+def generate_wallet_value(wallet_value: float, lance_value: float) -> float:
+    user = Usuario('User 1', wallet_value)
     leilao = Leilao('Celular')
 
-    user1.propoe_lance(leilao, 50.0)
+    user.propoe_lance(leilao, lance_value)
 
-    assert user1.carteira == 50.0
+    return user.carteira
+
+def test_subtract_wallet_value():
+    wallet = generate_wallet_value(100.0, 50.0)
+    assert wallet == 50.0
+
+
+def test_lance_value_less_than_wallet_value():
+    wallet = generate_wallet_value(100.0, 1.0)
+    assert wallet == 99.0
+
+
+def test_lance_value_equal_wallet_value():
+    wallet = generate_wallet_value(50.0, 50.0)
+    assert wallet == 0.0
+
+
+def test_doesnt_allow_lance_value_greater_than_wallet_value():
+    wallet = generate_wallet_value(50.0, 100.0)
+    assert wallet == 50.0
